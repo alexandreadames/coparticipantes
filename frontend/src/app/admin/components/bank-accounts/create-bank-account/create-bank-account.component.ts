fcdf4025eac9f3a411f695a7f928f9b6d18c4222
Alexandre <alexandre.adames@gmail.com>
2018-08-08 22:15:17 -0300

    Bank Account validator in front-end using moip account-bank-validator script

--- frontend/src/app/admin/components/bank-accounts/create-bank-account/create-bank-account.component.ts
+++ frontend/src/app/admin/components/bank-accounts/create-bank-account/create-bank-account.component.ts
@@ -1,5 +1,12 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import 'bank-account-validator/dist/bank-account-validator.min';
+
+declare var Moip: any;
+
+//Models
+import { BankAccount } from './../../../../models/BankAccount';
+
 
 //enviroment
 import { environment } from 'environments/environment';
@@ -12,6 +19,14 @@ import { environment } from 'environments/environment';
 export class CreateBankAccountComponent implements OnInit {
 
   banks: {};
+  bank: BankAccount = {
+    id_bank: 0,
+    agencyNumber: 0,
+    agencyCheckNumber: 0,
+    accountNumber: 0,
+    accountCheckNumber: 0,
+    id_account_type: 0
+  };
   selectedBank: number;
   baseApiUrl: string = environment.baseApiUrl;
   banksUrl: string = `${this.baseApiUrl}/secure/banks`;
@@ -39,6 +54,29 @@ export class CreateBankAccountComponent implements OnInit {
         }
   );
 
+  }
+
+  validate(){
+    //Use moip library account validator in front
+    Moip.BankAccount.validate({
+      bankNumber         : this.selectedBank.code,
+      agencyNumber       : this.bank.agencyNumber,
+      agencyCheckNumber  : this.bank.agencyCheckNumber,
+      accountNumber      : this.bank.accountNumber,
+      accountCheckNumber : this.bank.accountCheckNumber,
+      valid: function() {
+        alert("Conta bancária válida")
+      },
+      invalid: function(data) {
+        var errors = "Conta bancária inválida: \n";
+        for(let i in data.errors){
+          errors += data.errors[i].description + "-" + data.errors[i].code + ")\n";
+        }
+        alert(errors);
+      }
+    });
+    console.log("ACCOUNT DATA=>",this.bank);
+    console.log("SELECTED BANK=>", this.selectedBank);
   }
 
 }
---
+++ frontend/src/app/models/BankAccount.ts
@@ -0,0 +1,8 @@
+export interface BankAccount {
+  id_bank: number;
+  agencyNumber: number;
+  agencyCheckNumber: number
+  accountNumber: number;
+  accountCheckNumber: number;
+  id_account_type: number;
+}
